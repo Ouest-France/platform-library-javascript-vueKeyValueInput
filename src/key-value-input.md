b1677b627b19da00c582aa025829a3311027a9c0
Benoit Averty <benoit.averty@gmail.com>
2018-09-24 10:08:01 +0200

    feat: Handle the case of dupicate keys

--- src/key-value-input.md
+++ src/key-value-input.md
@@ -1,4 +1,4 @@
-key-value-input example:
+Basic example:
 
 ```vue
 <template>
@@ -21,6 +21,35 @@ require('./plugins/vuetify.js')
 export default {
   data: () => ({
     example: {}
+  })
+}
+</script>
+```
+
+Overriding error messages (type a duplicate to test): 
+```vue
+<template>
+  <div>
+    <key-value-input
+    keysLabel="K"
+    valuesLabel="V"
+    duplicateKeyErrorMessage="Dup key error"
+    duplicateValueErrorMessage=""
+    v-model="example"
+    />
+    <p>
+      {{ JSON.stringify(example) }}
+    </p>
+  </div>
+</template>
+
+<script>
+// Vuetify is needed to use this component. the the plugins/vuetify file of the repo if you need an example.
+require('./plugins/vuetify.js')
+
+export default {
+  data: () => ({
+    example: { duplicate: "Value" }
   })
 }
 </script>
--- src/key-value-input.vue
+++ src/key-value-input.vue
@@ -5,13 +5,15 @@
       <v-flex ml-1 v-if="valuesLabel" data-test="values-label"><span class="subheading">{{ valuesLabel }}</span></v-flex>
     </v-layout>
     <v-layout 
-      v-for="([k, v],i) in pairs"
+      v-for="([k, v],i) in templatePairs"
       :key="i"
     >
       <v-flex mr-1>
         <v-text-field 
           :placeholder="keysLabel"
           :value="k"
+          :error="duplicates.includes(i)"
+          :error-messages="duplicates.includes(i) ? duplicateKeyErrorMessage : null"
           @input="key => changeKey(i, key)"
           data-test="key-input"
         />
@@ -20,6 +22,8 @@
         <v-text-field
         :placeholder="valuesLabel"
         :value="v"
+        :error="duplicates.includes(i)"
+        :error-messages="duplicates.includes(i) ? duplicateValueErrorMessage : null"
         :disabled="k === ''"
         @input="value => changeValue(i, value)"
         data-test="value-input"
@@ -33,8 +37,34 @@
 const isString = v =>
   (v === null) | (v === undefined) | (typeof v === "string");
 
+// add a property with key/value to the given object only if the key isn't already present
+const addPropIfNotPresent = (obj, [key, value]) =>
+  obj.hasOwnProperty(key) ? obj : { ...obj, [key]: value };
+
+// Transform an array of key-value arrays into an object. The array is used as
+// the internal representation of data in the component.
+// The generated object keeps the first pair in case of duplicate keys.
 const objectFromPairs = pairs =>
-  pairs.reduce((acc, curr) => ({ ...acc, [curr[0]]: curr[1] }), {});
+  pairs.reduce((acc, curr) => addPropIfNotPresent(acc, curr), {});
+
+// Transform an object into an array of key-value arrays, which is used as the
+// internal representation of data in the component.
+const pairsFromObject = object => Object.entries(object);
+
+// function areEqualPairs(a, b) {
+//   if (a.length !== b.length) {
+//     return false;
+//   }
+
+//   for (let i = 0; i < a.length; ++i) {
+//     // Comparing keys and values for each pair
+//     if (a[i][0] !== b[i][0] || a[i][1] !== b[i][1]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
 
 function areEqualShallow(a, b) {
   for (let key in a) {
@@ -74,7 +104,24 @@ export default {
       default: null,
     },
     /**
+     * Error message used to signal duplicate keys.
+     */
+    duplicateKeyErrorMessage: {
+      type: String,
+      required: false,
+      default: "This key is a duplicate.",
+    },
+    /**
+     * Error message used on the value field of a duplicate key.
+     */
+    duplicateValueErrorMessage: {
+      type: String,
+      required: false,
+      default: "This value won't be used.",
+    },
+    /**
      * Value of the component. If not provided, the component will manage its own internal state.
+     * Value must be a shallow object of string keys / string values.
      */
     value: {
       type: Object,
@@ -88,67 +135,83 @@ export default {
     // This value is overriden with the prop at component creation and each time value prop changes.
     // This value should only be updated and read through the "effectiveValue" computed prop.
     return {
-      internalValue: this.value ? this.value : {},
+      internalValue: this.value ? pairsFromObject(this.value) : [],
     };
   },
   watch: {
     // Update the effective value when the value prop changes.
     value(newValue) {
-      if (!areEqualShallow(newValue, this.internalValue)) {
-        this.effectiveValue = newValue;
+      const currentValueAsObject = objectFromPairs(this.effectiveValue);
+
+      if (!areEqualShallow(newValue, currentValueAsObject)) {
+        this.effectiveValue = pairsFromObject(newValue);
       }
     },
   },
   computed: {
-    // This computed prop allows to safely read/write the value of the component. It handles the
-    // interactions between the value prop and the internalValue data.
+    // This computed prop allows to safely read/write the value of the component. It checks data equality
+    // and emits the "input" event when needed.
     effectiveValue: {
       get() {
         return this.internalValue;
       },
       set(v) {
+        const newValueAsObject = objectFromPairs(v);
+        const currentValueAsObject = objectFromPairs(this.internalValue);
+
         this.internalValue = v;
-
-        /**
-         * Event triggered at each value change.
-         *
-         * @event input
-         * @type {object}
-         */
-        this.$emit("input", v);
-      },
-    },
-    // The value as a list of pairs. Each pair is an array of the shape [key, value].
-    // This is easier to manipulate from the template.
-    pairs: {
-      get() {
-        return Object.entries(this.effectiveValue).concat([["", ""]]);
-      },
-      set(newPairs) {
-        const newValue = objectFromPairs(newPairs.filter(([k]) => k));
-
-        if (!areEqualShallow(newValue, this.effectiveValue)) {
-          this.effectiveValue = newValue;
+        if (!areEqualShallow(newValueAsObject, currentValueAsObject)) {
+          /**
+           * Event triggered at each value change.
+           *
+           * @event input
+           * @type {object}
+           */
+          this.$emit("input", objectFromPairs(v));
         }
       },
+    },
+    // List of pairs to be handled in the template. It adds an empty key/value to generate the row
+    // that allows to create a key, and removes any empty key before saving.
+    templatePairs: {
+      get() {
+        return this.effectiveValue.concat([["", ""]]);
+      },
+      set(newPairs) {
+        const newValue = newPairs.filter(([k]) => k);
+
+        this.effectiveValue = newValue;
+      },
+    },
+    // List of pairs indices that are duplicates.
+    duplicates() {
+      return this.effectiveValue
+        .map(v => v[0]) // keep only keys to find duplicates.
+        .map(
+          // If the first element of the array is not the current element itself,
+          // that means it's a duplicate.
+          (value, idx, array) =>
+            array.findIndex(el => el === value) < idx ? idx : null,
+        )
+        .filter(el => el !== null);
     },
   },
   methods: {
     // Updates the key of the pair at the given index
     changeKey(index, key) {
-      const head = this.pairs.slice(0, index);
-      const updatedPair = [key, this.pairs[index][1]];
-      const tail = this.pairs.slice(index + 1);
+      const head = this.templatePairs.slice(0, index);
+      const updatedPair = [key, this.templatePairs[index][1]];
+      const tail = this.templatePairs.slice(index + 1);
 
-      this.pairs = head.concat([updatedPair]).concat(tail);
+      this.templatePairs = head.concat([updatedPair]).concat(tail);
     },
     // Updates the value of the pair at the given index
     changeValue(index, value) {
-      const head = this.pairs.slice(0, index);
-      const updatedPair = [this.pairs[index][0], value];
-      const tail = this.pairs.slice(index + 1);
+      const head = this.templatePairs.slice(0, index);
+      const updatedPair = [this.templatePairs[index][0], value];
+      const tail = this.templatePairs.slice(index + 1);
 
-      this.pairs = head.concat([updatedPair]).concat(tail);
+      this.templatePairs = head.concat([updatedPair]).concat(tail);
     },
   },
 };
